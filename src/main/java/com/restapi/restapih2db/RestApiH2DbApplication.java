60ab29382a1c2b36368fc691e5b16b21ab4238af
cgaswin <cgaswin333@gmail.com>
2024-07-23 21:15:04 +0530

    iniitial setup

---
+++ src/main/java/com/restapi/restapih2db/RestApiH2DbApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.restapih2db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiH2DbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiH2DbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/restapih2db/RestApiH2DbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.restapih2db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiH2DbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
